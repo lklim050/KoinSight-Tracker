37e31ec791dd567d140be9eb0aec4c9299f4536b
Kenneth <wongyxk@hotmail.com>
2026-05-23 19:59:06 +0800

    Update auth.js

--- backend/src/routers/auth.js
+++ backend/src/routers/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { protect } from "../middlewares/auth.js";
 import { signup, login } from "../controllers/auth.js";
 
 const router = express.Router();
@@ -7,5 +7,13 @@ const router = express.Router();
 router.post("/signup", signup);
 
 router.post("/login", login);
+
+router.get("/profile", protect, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+
+    user: req.user,
+  });
+});
 
 export default router;
