c3b216920997d0616aee371a376ed6991831ddb4
Lincoln <lklim050@gmail.com>
2026-07-14 14:45:57 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,11 @@ mongoose
 
 connectDB();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", // for deploy FRONTEND URL is the Render env
+  }),
+);
 app.use(
   helmet({
     crossOriginResourcePolicy: { policy: "cross-origin" },
